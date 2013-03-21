0341d1d70ad242702567480801e6eafb86cfe33b
manuroot <mroot72000@yahoo.fr>
2013-03-21 21:56:47 +0100

    Day 3: sonataadmin: news tinymce

--- web/app.php
+++ web/app.php
@@ -8,10 +8,10 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
+
+$loader = new ApcClassLoader('blogsf2', $loader);
 $loader->register(true);
-*/
+
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
@@ -24,3 +24,10 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+
+/*Couple more things to consider:
+
+php app/console cache:clear --env=prod --no-debug
+php app/console assets:install web_directory
+php app/console assetic:dump web_directory*/
