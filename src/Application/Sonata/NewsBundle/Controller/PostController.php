afb0fc0bd5b149aae1efcb4979f9ff2df4aee9c5
manuroot <mroot72000@yahoo.fr>
2013-04-09 22:06:23 +0200

    eservice contrat de service

--- src/Application/Sonata/NewsBundle/Controller/PostController.php
+++ src/Application/Sonata/NewsBundle/Controller/PostController.php
@@ -20,6 +20,7 @@ use Sonata\NewsBundle\Model\CommentInterface;
 use Sonata\NewsBundle\Model\PostInterface;
 use Doctrine\ORM\EntityRepository;
 use Sonata\NewsBundle\Controller\PostController as Controller;
+
 class PostController extends Controller {
 
     /**
@@ -33,53 +34,87 @@ class PostController extends Controller {
 
         $em = $this->container->get('doctrine')->getEntityManager();
         $alltags = $em->getRepository('ApplicationSonataNewsBundle:Tag')->findByEnabled(1);
-             //   ->findAll();
-      return ($alltags);
+        //   ->findAll();
+         // $tagWeights = $em->getRepository('ApplicationSonataNewsBundle:Tag')->getTagWeights($alltags);
+      
+        //getPosts()
+        $tagWeights=array();
+        $aa=array();
+        foreach ($alltags as $tag)
+    {
+        
+       // $tagWeights[$alltags] =1;
+        $a=$tag->getPosts();
+       // $b=0;
+        $b=count($a);
+        //$aa[$tag->getName()]=$b;
+         $tagWeights[$tag->getName()] =$b;
+       //   $tagWeights[$tag] =$b;
     }
- 
+ // Shuffle the tags
+   /* uksort($tagWeights, function() {
+        return rand() > rand();
+    }); */   
+     $max = max($tagWeights);
+
+    // Max of 5 weights
+    $multiplier = ($max > 5) ? 5 / $max : 1;
+    foreach ($tagWeights as &$tag)
+    {
+        $tag = ceil($tag * $multiplier);
+    }
+
+   // print_r($tagWeights);
+    //exit(1);
+      return array($alltags,$tagWeights);
+    }
+
     private function sidebar_comments() {
         $em = $this->container->get('doctrine')->getEntityManager();
         //myFindAll
-      //  $allcategories = $em->getRepository('ApplicationSonataNewsBundle:Category')->myFindAll();
-       $lastcomments = $em->getRepository('ApplicationSonataNewsBundle:Comment')->FindLastComments();
-            //    ->findAll();
-       return ($lastcomments);
+        //  $allcategories = $em->getRepository('ApplicationSonataNewsBundle:Category')->myFindAll();
+        $lastcomments = $em->getRepository('ApplicationSonataNewsBundle:Comment')->FindLastComments();
+        //    ->findAll();
+        return ($lastcomments);
     }
-   private function sidebar_categories() {
+
+    private function sidebar_categories() {
         $em = $this->container->get('doctrine')->getEntityManager();
         //myFindAll
-      //  $allcategories = $em->getRepository('ApplicationSonataNewsBundle:Category')->myFindAll();
-       $allcategories = $em->getRepository('ApplicationSonataNewsBundle:Category')->findByEnabled(1);
-            //    ->findAll();
-       return ($allcategories);
+        //  $allcategories = $em->getRepository('ApplicationSonataNewsBundle:Category')->myFindAll();
+        $allcategories = $em->getRepository('ApplicationSonataNewsBundle:Category')->findByEnabled(1);
+        //    ->findAll();
+        return ($allcategories);
     }
+
     /**
      * @param array $criteria
      *
      * @return Response
      */
     public function renderArchive(array $criteria = array(), array $parameters = array()) {
-        
-         $form_paypal = $this->createPurchaseForm();
+
+        $form_paypal = $this->createPurchaseForm();
         $pager = $this->getPostManager()->getPager(
-                $criteria, $this->getRequest()->get('page', 1),5
+                $criteria, $this->getRequest()->get('page', 1), 5
         );
         $test = "(surcharge du controleur: phase de développement)";
-          $alltags = $this->sidebar_tags();
-          $allcategories = $this->sidebar_categories();
-          $lastcomments=$this->sidebar_comments();
+        list($alltags,$tagWeights) = $this->sidebar_tags();
+        $allcategories = $this->sidebar_categories();
+        $lastcomments = $this->sidebar_comments();
         // $alltags=$em->getRepository('ApplicationSonataNewsBundle:Tag')->findAll();
-         // $pager->setLinks(3);
-         $parameters = array_merge(array(
-          'pager' => $pager,
-          'blog'  => $this->get('sonata.news.blog'),
-          'tag'   => false,
-          'test'=> $test,
-          'form_paypal' => $form_paypal->createView(),
-          'allcategories' => $allcategories,
-          'alltags' => $alltags,
-           'lastcomments' =>  $lastcomments,
-          ), $parameters); 
+        // $pager->setLinks(3);
+        $parameters = array_merge(array(
+            'pager' => $pager,
+            'blog' => $this->get('sonata.news.blog'),
+            'tag' => false,
+            'test' => $test,
+            'form_paypal' => $form_paypal->createView(),
+            'allcategories' => $allcategories,
+            'alltags' => $alltags,
+            'lastcomments' => $lastcomments,
+            'tagweight' => $tagWeights,
+                ), $parameters);
         $response = $this->render(sprintf('SonataNewsBundle:Post:archive.%s.twig', $this->getRequest()->getRequestFormat()), $parameters);
 
         if ('rss' === $this->getRequest()->getRequestFormat()) {
@@ -89,43 +124,40 @@ class PostController extends Controller {
         return $response;
     }
 
-    
-    
-      public function mesnewsAction(array $criteria = array(), array $parameters = array()) {
-        
+    public function mesnewsAction(array $criteria = array(), array $parameters = array()) {
+
         $form_paypal = $this->createPurchaseForm();
-       /*  $mypager = $this->getPostManager()-> getPager(
-                $criteria, $this->getRequest()->get('page', 1)
-        );*/
-         //$mypager->getResult();
-        /*$pager = $this->getPostManager()-> getPagerquery(
-                $criteria, $this->getRequest()->get('page', 1)
-        );*/
-         $em = $this->getDoctrine()->getManager();
+        /*  $mypager = $this->getPostManager()-> getPager(
+          $criteria, $this->getRequest()->get('page', 1)
+          ); */
+        //$mypager->getResult();
+        /* $pager = $this->getPostManager()-> getPagerquery(
+          $criteria, $this->getRequest()->get('page', 1)
+          ); */
+        $em = $this->getDoctrine()->getManager();
         $query = $em->getRepository('ApplicationSonataNewsBundle:Post')->myFindAll();
-          $alltags = $this->sidebar_tags();
-          $allcategories = $this->sidebar_categories();
-      
-            $paginator = $this->get('knp_paginator');
+     //   $alltags = $this->sidebar_tags();
+         list($alltags,$tagWeights) = $this->sidebar_tags();
+    
+        $allcategories = $this->sidebar_categories();
+        $lastcomments = $this->sidebar_comments();
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
                 $query, $this->get('request')->query->get('page', 1)/* page number */, 10/* limit per page */
         );
-   
-     $pagination->setTemplate('ApplicationSonataNewsBundle:pagination:twitter_bootstrap_pagination.html.twig');
-     
-         return $this->render('ApplicationSonataNewsBundle:Post:mesnews.html.twig',
-                array(
-                   // 'pager' => $mypager,
-                'pagination' => $pagination,
-                       'form_paypal' => $form_paypal->createView(),
-                       'allcategories' => $allcategories,
-          'alltags' => $alltags,
-      
-      ));
-   
-    }
 
-  
+        $pagination->setTemplate('ApplicationSonataNewsBundle:pagination:twitter_bootstrap_pagination.html.twig');
+
+        return $this->render('ApplicationSonataNewsBundle:Post:mesnews.html.twig', array(
+                    // 'pager' => $mypager,
+                    'pagination' => $pagination,
+                    'form_paypal' => $form_paypal->createView(),
+                    'allcategories' => $allcategories,
+                    'lastcomments' => $lastcomments,
+                    'alltags' => $alltags,
+             'tagweight' => $tagWeights,
+                ));
+    }
 
     /**
      * @throws NotFoundHttpException
@@ -141,9 +173,11 @@ class PostController extends Controller {
         if (!$post || !$post->isPublic()) {
             throw new NotFoundHttpException('Unable to find the post');
         }
-   $alltags = $this->sidebar_tags();
-          $allcategories = $this->sidebar_categories();
-       
+     //   $alltags = $this->sidebar_tags();
+          list($alltags,$tagWeights) = $this->sidebar_tags();
+     
+        $allcategories = $this->sidebar_categories();
+         $lastcomments = $this->sidebar_comments();
         if ($seoPage = $this->getSeoPage()) {
             $seoPage
                     ->setTitle($post->getTitle())
@@ -156,45 +190,46 @@ class PostController extends Controller {
                     ->addMeta('property', 'og:description', $post->getAbstract())
             ;
         }
-/*
-        
-        ADD: tout sur la meme page
-        */
- 
-        $page=$this->getRequest()->get('page', 1);
-        $pager = $this->getCommentManager()
-            ->getPager(array(
-                'postId' => $post->getId(),
-                'status'  => CommentInterface::STATUS_VALID
-            ), $page, 5); //no limit
+        /*
 
-       
-        
+          ADD: tout sur la meme page
+         */
+
+        $page = $this->getRequest()->get('page', 1);
+        $pager = $this->getCommentManager()
+                ->getPager(array(
+            'postId' => $post->getId(),
+            'status' => CommentInterface::STATUS_VALID
+                ), $page, 5); //no limit
+
+
+
         return $this->render('SonataNewsBundle:Post:view.html.twig', array(
                     'post' => $post,
                     'form' => false,
                     'blog' => $this->get('sonata.news.blog'),
                     'form_paypal' => $form_paypal->createView(),
-              'pager'  => $pager,
-              'allcategories' => $allcategories,
-          'alltags' => $alltags,
+                    'pager' => $pager,
+                    'allcategories' => $allcategories,
+                    'alltags' => $alltags,
+             'lastcomments' => $lastcomments,
+               'tagweight' => $tagWeights,
       
                 ));
     }
 
-     /**
+    /**
      * @param $category
      *
      * @return \Symfony\Component\HttpFoundation\Response
      *
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
-    public function categoryAction($category)
-    {
+    public function categoryAction($category) {
         $category = $this->get('sonata.news.manager.category')->findOneBy(array(
             'slug' => $category,
             'enabled' => true
-        ));
+                ));
 
         if (!$category) {
             throw new NotFoundHttpException('Unable to find the category');
@@ -207,67 +242,63 @@ class PostController extends Controller {
         return $this->renderArchive(array('category' => $category), array('category' => $category));
     }
 
-     /**
+    /**
      * @param integer $postId
      *
      * @return Response
      */
-    public function mycommentsAction($mpost,$postId)
-            {
+    public function mycommentsAction($mpost, $postId) {
         $pager = $this->getCommentManager()
-            ->getPager(array(
-                'postId' => $postId,
-                'status'  => CommentInterface::STATUS_VALID
-            ), 1, 5); //no limit
+                ->getPager(array(
+            'postId' => $postId,
+            'status' => CommentInterface::STATUS_VALID
+                ), 1, 5); //no limit
 
         return $this->render('SonataNewsBundle:Post:comments.html.twig', array(
-            'pager'  => $pager,
-            'mpost' => $mpost,
-        ));
+                    'pager' => $pager,
+                    'mpost' => $mpost,
+                ));
     }
-    
-    public function commentsAction($postId)
-    {
-         $pager = $this->getCommentManager()
-            ->getPager(array(
-                'postId' => $postId,
-                'status'  => CommentInterface::STATUS_VALID
-            ), 1, 500); //no limit
+
+    public function commentsAction($postId) {
+        $pager = $this->getCommentManager()
+                ->getPager(array(
+            'postId' => $postId,
+            'status' => CommentInterface::STATUS_VALID
+                ), 1, 500); //no limit
 
         return $this->render('SonataNewsBundle:Post:comments.html.twig', array(
-            'pager'  => $pager,
-             'postId' => $postId,
-           
-        ));
+                    'pager' => $pager,
+                    'postId' => $postId,
+                ));
     }
-    
-     /**
+
+    /**
      * @param $postId
      * @param bool $form
      *
      * @return Response
      */
     public function addCommentFormAction($postId, $form = false) {
-    //    parent::addCommentFormAction($postId, $form=false);
+        //    parent::addCommentFormAction($postId, $form=false);
         if (!$form) {
             $post = $this->getPostManager()->findOneBy(array(
                 'id' => $postId
                     ));
-       /*$em = $this->getDoctrine()->getManager();
-        $user = $this->get('security.context')->getToken()->getUser();
-        $user_security = $this->container->get('security.context');
-        //if( $user_security->isGranted('IS_AUTHENTICATED_REMEMBERED') ){
-      if ($user_security->isGranted('IS_AUTHENTICATED_FULLY')) {
-             $user_id = $user->getId();
-        
+            /* $em = $this->getDoctrine()->getManager();
+              $user = $this->get('security.context')->getToken()->getUser();
+              $user_security = $this->container->get('security.context');
+              //if( $user_security->isGranted('IS_AUTHENTICATED_REMEMBERED') ){
+              if ($user_security->isGranted('IS_AUTHENTICATED_FULLY')) {
+              $user_id = $user->getId();
+
               $current_user = $em->getRepository('ApplicationSonataUserBundle:User')->find($user_id);
-        $post->setDemandeur($current_user);
+              $post->setDemandeur($current_user);
 
-             
-        }*/
+
+              } */
             $form = $this->getCommentForm($post);
-          //  $form->setData(array('email'=>'email'));
-
+            //  $form->setData(array('email'=>'email'));
         }
 
         return $this->render('SonataNewsBundle:Post:comment_form.html.twig', array(
@@ -276,8 +307,6 @@ class PostController extends Controller {
                 ));
     }
 
-   
-  
     protected function createPurchaseForm() {
 
         return $this->createFormBuilder()
---
+++ src/Application/Sonata/NewsBundle/Entity/TagRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of the <name> project.
+ *
+ * (c) <yourname> <youremail>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\Sonata\NewsBundle\Entity;
+use Sonata\NewsBundle\Entity\BasePostRepository;
+
+class TagRepository extends BasePostRepository
+{
+
+    public function myFindAll() {
+        return $this->createQueryBuilder('p')
+                
+                //  ->add('orderBy', 'p.id DESC')
+                 ->where('p.enabled = 1')
+               ->orderby('p.createdAt', 'DESC')
+                        ->getQuery();
+
+        //->getResult();
+    }
+public function getTags()
+{
+    $blogTags = $this->createQueryBuilder('b')
+                     ->select('b.tags')
+                     ->getQuery()
+                     ->getResult();
+
+    $tags = array();
+    foreach ($blogTags as $blogTag)
+    {
+   //     $tags = array_merge(explode(",", $blogTag['tags']), $tags);
+            $tags = $blogTag['tags'];
+    }
+
+    foreach ($tags as &$tag)
+    {
+        $tag = trim($tag);
+    }
+
+    return $tags;
+}
+
+public function getTagWeights($tags)
+{
+    $tagWeights = array();
+    if (empty($tags))
+        return $tagWeights;
+
+    foreach ($tags as $tag)
+    {
+        
+        $tagWeights[$tag] =count($tags->getPosts());
+    }
+    // Shuffle the tags
+    uksort($tagWeights, function() {
+        return rand() > rand();
+    });
+
+    $max = max($tagWeights);
+
+    // Max of 5 weights
+    $multiplier = ($max > 5) ? 5 / $max : 1;
+    foreach ($tagWeights as &$tag)
+    {
+        $tag = ceil($tag * $multiplier);
+    }
+
+    return $tagWeights;
+}
+
+}
